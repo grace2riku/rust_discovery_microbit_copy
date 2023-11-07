7d71b3c631f34033b98f909a6868f82707c203f7
Koji Abe <gracehime@gmail.com>
2023-11-07 23:31:18 +0900

    5.6. The challenge DisplayのAPIでLED全点灯、全消灯を確認

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -3,28 +3,34 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
+use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
-use microbit::board::Board;
-use microbit::hal::timer::Timer;
-use microbit::hal::prelude::*;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    let mut board = Board::take().unwrap();
 
+    let board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
-
-    board.display_pins.col1.set_low().unwrap();
-    let mut row1 = board.display_pins.row1;
+    let mut display = Display::new(board.display_pins);
+    let light_it_all = [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+    ];
 
     loop {
-        row1.set_low().unwrap();
-        rprintln!("Dark!");
-        timer.delay_ms(1_000_u16);
-        row1.set_high().unwrap();
-        rprintln!("Light!");
-        timer.delay_ms(1_000_u16);
+        // Show light_it_all for 1000ms
+        display.show(&mut timer, light_it_all, 1000);
+        // clear the display again
+        display.clear();
+        timer.delay_ms(1000_u32);
     }
 }
