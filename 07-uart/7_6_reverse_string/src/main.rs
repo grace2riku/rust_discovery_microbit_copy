3f2140f700330d743e108c0c0cdce297784da16b
Koji Abe <gracehime@gmail.com>
2023-11-18 23:14:59 +0900

    7_6 Reverse a stringのコードを実装

---
+++ 07-uart/7_6_reverse_string/src/main.rs
@@ -0,0 +1,69 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+use core::fmt::Write;
+use heapless::Vec;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+
+#[cfg(feature = "v1")]
+use microbit::{
+    hal::prelude::*,
+    hal::uart,
+    hal::uart::{Baudrate, Parity},
+};
+
+#[cfg(feature = "v2")]
+use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+};
+
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = microbit::Board::take().unwrap();
+
+    #[cfg(feature = "v1")]
+    let mut serial = {
+        uart::Uart::new(
+            board::Uart::new(
+                board.UART0,
+                board.uart.into(),
+                Parity::EXCLUDED,
+                Baudrate::BAUD115200,
+            )
+        )
+    };
+
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
+    loop {
+        buffer.clear();
+
+        // TODO Receive a user request. Each user request ends with Enter
+        // NOTE 'buffer.push' returns a 'Result'. Handle the error by responding
+        // with an error message.
+
+        // TODO Send back the reversed string
+    }
+}
